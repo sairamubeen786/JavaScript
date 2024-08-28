c812cbc79cda6d82ad12b00267836de0d0fdb1db
Javascript <seoseller404@gmail.com>
2024-08-28 11:22:15 -0700

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,12 @@
+var myname = "saira";
+var myname="mubeen"
+console.log(myname);
+
+var myEmail = "saira@gmail.com";
+    myEmail="mubeen@gmail.com"
+console.log(myEmail);
+
+const id = "2";
+console.table([id,myname,myEmail ]);
+
+alert("This table was created ");
